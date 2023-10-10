644f2059644ec050f47d80135e34f516fc8a4352
Sephaniaalx <koutanyi@gmail.com>
2023-10-09 20:47:31 -0700

    9-max_integer.py

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if not my_list:  # Check if the list is empty
+        return None
+
+    max_num = my_list[0]  # Initialize max_num
+
+    for num in my_list:
+        if num > max_num:
+            max_num = num
+
+    return max_num
