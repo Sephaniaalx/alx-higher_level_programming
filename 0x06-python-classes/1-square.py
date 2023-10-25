edcfe3b5e0582e48285f4401c4d983ed47c752d7
Sephaniaalx <122804994+Sephaniaalx@users.noreply.github.com>
2023-10-25 06:12:00 +0100

    done and tested

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a square """
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size):
+        """Initializing this square class
+        Args: size - This represents the size of the square
+        """
+
+        self.__size = size
