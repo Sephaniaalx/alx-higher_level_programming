159fe362a601c53879eea37259445e16575636b3
Sephaniaalx <koutanyi@moolre.com>
2023-12-13 10:40:44 -0800

    1-create_database_if_missing.sql

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -2,5 +2,5 @@
 -- because Batch 3 is the best!
 -- SELECT id, name FROM students WHERE batch_id = 3 ORDER BY created_at DESC LIMIT 3;
 
-CREATE DATABASE hbtn_0c_0 IF NOT EXISTS;
+CREATE DATABASE hbtn_0c_0;
 
