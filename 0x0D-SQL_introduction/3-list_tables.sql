63a5ec2abb18d286bb949bed1fe0f5ecdb0c8ebd
Sephaniaalx <koutanyi@moolre.com>
2023-12-13 11:03:44 -0800

    hbtn_0c_0

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,6 @@
+-- 3 first students in the Batch ID=3
+-- because Batch 3 is the best!
+-- SELECT id, name FROM students WHERE batch_id = 3 ORDER BY created_at DESC LIMIT 3;
+
+SHOW TABLES FROM hbtn_0c_0;
+
