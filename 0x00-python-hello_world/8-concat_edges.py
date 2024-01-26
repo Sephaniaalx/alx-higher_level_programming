023eadaab7f7e635fefc06855a49cb9d40c9264e
Sephaniaalx <koutanyi@moolre.com>
2024-01-26 00:02:48 -0800

    8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -2,5 +2,4 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 print(f"{str[39:67]}", end= "")
-print(f"{str[107:112]}", end="")
-print(f"{str[0:6]}")
+print(f"{str[107:112]}{str[0:6]}")
